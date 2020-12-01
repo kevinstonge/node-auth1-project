082b3bb4192d42eb5f089363e6e53f98be6a6d70
Kevin St.Onge <kevinstonge@gmail.com>
2020-12-01 13:12:24 -0500

    no significant change

--- server.js
+++ server.js
@@ -11,16 +11,17 @@ server.use(
     name: 'authentication-project',
     secret: 'forty-two',
     cookie: {
-      maxAge: 1 * 24 * 60 * 60 * 1000,
-      secure: true, // only set cookies over https. Server will not send back a cookie over http.
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        secure: false, // only set cookies over https. Server will not send back a cookie over http.
+        httpOnly: true,
     }, // 1 day in milliseconds
-    // httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
     resave: true,
     saveUninitialized: true,
   })
 );
 
 const usersRouter = require('./api/users/usersRouter.js');
+
 server.use('/api/users', usersRouter);
 server.get('/', (req, res) => { res.status(200).json({ message: "server online" }); });
 
